e72753b8c102c5074956c716fb967efd0be902ac
aravindh <aravindhfinix@gmail.com>
2022-03-18 14:36:17 +0530

    updated with get users

--- expresspractice/index.js
+++ expresspractice/index.js
@@ -1,14 +1,21 @@
 const express=require('express');
 const app=express();
 
+
 app.get('/',(req,res)=>{
 res.send("hi friends")
 });
-app.get('/no1',(req,res)=>{
-    res.send("hi friends")
-    });
-    
+    app.get('/function',(req,res)=>
+    {res.sendFile(`${__dirname}/main.html`)})
+    app.get('/function/:year/:month',(req,res)=>{
+        res.send(req.params)
+        });
+        app.get('/function/name',(req,res)=>{
+            res.send(req.query)
+            });         
+  //importing find user via get      
 const ownrouter=require("./myroute")
-app.use('/test',ownrouter)
+app.use('/users',ownrouter)
 
-app.listen(8080,()=>{console.log("start the server at 8080")});
+const port=process.env.PORT||3000
+app.listen(port,()=>console.log(`start the server at ${port}`));
--- expresspractice/myroute.js
+++ expresspractice/myroute.js
@@ -1,11 +1,19 @@
 const express=require('express');
 const router=express.Router();
+const users=[
+    {id:1 ,person:'hari'},
+    {id:2 ,person:'bala'},
+    {id:3 ,person:'mohan'},
+]
+
 
 router.get('/',(req,res)=>{
-    res.json("roiuter is working");
+    res.send(users);
     });
-    router.get('/no1',(req,res)=>{
-        res.json("roiuter no1 is working");
+    router.get('/:id',(req,res)=>{
+        const user=users.find(c=>c.id===parseInt(req.params.id));
+        if(!user)res.status(404).send('the requested user is not found!!!!!!!!1');
+        res.send(user);
         });
        module.exports=router;
         
