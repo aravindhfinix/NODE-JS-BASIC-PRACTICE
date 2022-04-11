05538aa7a395e1d0537cd3d45e8a91fd3970ee00
aravindh <aravindhfinix@gmail.com>
2022-04-11 23:14:40 +0530

    es6 fs

--- fs/fspractice length.js
+++ fs/fspractice length.js
@@ -1,9 +1,6 @@
-var fs = require('fs');
+const fs = require('fs');
 console.log('Starting program...');
-// Read a small file synchronously
-var fileContents = fs.readFileSync('words.txt', 'utf8');
-// Count number of words
-var numberOfWords = fileContents.split(/[ ,.\n]+/).length;
-// Print to console
-console.log('There are ' + numberOfWords + ' words in this file.');
+let fileContents = fs.readFileSync('words.txt', 'utf8');
+let numberOfWords = fileContents.split(/[ ,.\n]+/).length;
+console.log(`There are ${numberOfWords}  words in this file`);
 console.log('Program finished.');
