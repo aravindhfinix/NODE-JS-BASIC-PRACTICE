91b4f80fbd3a59885a95cf33b3a8eb4882c71388
aravindh <aravindhfinix@gmail.com>
2022-04-11 09:50:02 +0530

    cipher change

--- crypto/encoding.js
+++ crypto/encoding.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');  
-const cipher = crypto.createCipher('aes192', 'a password');  
+const cipher = crypto.Cipher('aes192', 'a password');  
 var encrypted = cipher.update('Hello JavaTpoint', 'utf8', 'hex');  
 encrypted += cipher.final('hex');  
 console.log(encrypted);
