b76421b14a8c343fa80d36cc3ff6bb264c526237
aravindh <aravindhfinix@gmail.com>
2022-04-28 18:07:28 +0530

    stripe

---
+++ Oauth/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const passport=require('passport');
+const passportgoogle=require('passport-google-oauth20');
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
---
+++ stripe api/index.js
@@ -0,0 +1,62 @@
+const express = require('express') 
+const bodyparser = require('body-parser') 
+const path = require('path') 
+const app = express() 
+
+var Publishable_Key = 'pk_test_51KtRjdSEpj3lanVG0SLIyT4H7TBJuJLFn7HoCAXau4nu6rRruJYA2WsTlGsrLavFUQYDh0c7kkML3aHplxsycVlT00atlLNuRY'
+var Secret_Key = 'sk_test_51KtRjdSEpj3lanVGH19bhI8EydadbNwf0wLzEQS5htf2nmtK4h7pHOUc848KYrMXk6J2tk2J7oDxYqYvWySLq8c000A6yF56OL'
+
+const stripe = require('stripe')(Secret_Key) 
+
+const port = process.env.PORT || 3000 
+
+app.use(bodyparser.urlencoded({extended:false})) 
+app.use(bodyparser.json()) 
+
+// View Engine Setup 
+app.set('views', path.join(__dirname, 'views')) 
+app.set('view engine', 'ejs') 
+
+app.get('/', function(req, res){ 
+    res.render('Home', { 
+    key: Publishable_Key 
+    }) 
+}) 
+
+app.post('/payment', function(req, res){ 
+
+    // Moreover you can take more details from user 
+    // like Address, Name, etc from form 
+    stripe.customers.create({ 
+        email: req.body.stripeEmail, 
+        source: req.body.stripeToken, 
+        name: 'mohana', 
+        address: { 
+            line1: 'TC 9/4 Old MES colony', 
+            postal_code: '110092', 
+            city: 'New Delhi', 
+            state: 'Delhi', 
+            country: 'India', 
+        } 
+    }) 
+    .then((customer) => { 
+
+        return stripe.charges.create({ 
+            amount: 70,    // Charing Rs 25 
+            description: 'Web Development Product', 
+            currency: 'INR', 
+            customer: customer.id 
+        }); 
+    }) 
+    .then((charge) => { 
+        res.send("Success") // If no error occurs 
+    }) 
+    .catch((err) => { 
+        res.send(err)    // If some error occurs 
+    }); 
+}) 
+
+app.listen(port, function(error){ 
+    if(error) throw error 
+    console.log("Server created Successfully") 
+})
