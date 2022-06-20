b1fe87cde8f9bd26d683de14e54a47c87f6478de
aravindh <aravindhfinix@gmail.com>
2022-06-20 12:07:27 +0530

    socket

--- sockets/index.js
+++ sockets/index.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
+
+  //send the index.html file for all requests
   res.sendFile(__dirname + '/index.html');
+
 });
 
+//client connection to the server
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect',()=>{
+    console.log('user disconnected')
+  })
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(3001, function(){
+
+  console.log('listening on 3001.....');
+
 });
