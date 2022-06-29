8f203633c939868ab5937b84ff3d01892133cc53
aravindh <aravindhfinix@gmail.com>
2022-06-29 18:46:51 +0530

    simple eod

--- Todo list api/src/controllers/todo.js
+++ Todo list api/src/controllers/todo.js
@@ -205,69 +205,102 @@ else{
 }
 }
 
-//EOD TASK STATUS EMAIL
+//EOD TASK STATUS EMAIL FOR RESPECTED USERS
 
-exports.mail=async(req,res)=>{
+// exports.mail=async(req,res)=>{
 
-await todoSchema.aggregate([
-    {$match:{taskCreatedAt:dateToday}},
-    {
-    $lookup: {
-        from: "users",
-        localField: "userId",
-        foreignField: "_id",
-        as: "uemail"
-    }}, 
-{$unwind: "$uemail"}
-])
+// await todoSchema.aggregate([
+//     {$match:{taskCreatedAt:dateToday}},
+//     {
+//     $lookup: {
+//         from: "users",
+//         localField: "userId",
+//         foreignField: "_id",
+//         as: "uemail"
+//     }}, 
+// {$unwind: "$uemail"}
+// ])
 
-.then(results=>{
-    cron.schedule('00 00 18 * * *',()=>{            //sheduled for every day 6PM
-if(results.length!=0){
-    let grouped = _.reduce(results, (result, user) => {
+// .then(results=>{
 
-    (result[user.uemail.email] || (result[user.uemail.email] = [])).push(user);
-    return result;
-}, {});
+//     cron.schedule('00 00 18 * * *',()=>{            //sheduled for every day 6PM
+// if(results.length!=0){
+//     let grouped = _.reduce(results, (result, user) => {
+//     (result[user.uemail.email] || (result[user.uemail.email] = [])).push(user);
+//     return result;
+// }, {});
 
-   
-        for(var i in grouped){
-            const email=i
-            const array=grouped[i]
-            var a1=[].splice(',')
-           var no=0
-            for(var d in array){
-                var task=array[d].taskName
-                if(array[d].status===true){
-                var status="completed"
-                }
-                else{
-                var status="not-completed"
-                }
-                var name=array[d].userName
-                no++
-                a1.push(`${no}.task ${task} is ${status}`)
-            }
-            var a2=a1.join('\r\n')
+//         for(var i in grouped){
+//             const email=i
+//             const array=grouped[i]
+//             var a1=[].splice(',')
+//            var no=0
+//             for(var d in array){
+//                 var task=array[d].taskName
+//                 if(array[d].status===true){
+//                 var status="completed"
+//                 }
+//                 else{
+//                 var status="not-completed"
+//                 }
+//                 var name=array[d].userName
+//                 no++
+//                 a1.push(`${no}.task ${task} is ${status}`)
+//             }
+//             var taskDetail=a1.join('\r\n')
     
-            sendMail(email,name,a2)           //invoking mail function
-            }
+//             sendMail(email,name,taskDetail)           //invoking mail function
+//             }
          
-            res.json({message:'EOD status sent'})
-        }
-else{
-    res.json({message:'no tasks found'})
-    console.log('no task')
-    }
-      })
-        } )
+//             res.json({message:'EOD status sent'})
+//         }
+// else{
+//     console.log('no task')
+//     res.json({message:'no tasks found'})
+//     }
+//       })
+//         } )
     
         
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error : err.message
-            })
-        });
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json({
+//                 error : err.message
+//             })
+//         });
     
+// } 
+
+
+exports.mail=async(req,res)=>{
+    await userSchema.find()
+    .then(async results=>{
+        results.forEach(async i=>{
+           var email=i.email
+           var name=i.name
+           var task1=[]
+           var no=0
+           var task=await todoSchema.find({userId:i._id})
+           if(task.length===0){
+            var taskStatus='no task today'
+         }else{
+            for(var j of task){
+                no++
+                var tasks=j.taskName
+                if(j.status===true){
+                    var status1=`${no}.your task ${tasks} is completed`
+                }
+                else{
+                    var status1=`${no}.your task ${tasks} is not-completed`
+                }
+                
+                task1.push(status1)
+            }
+            var taskStatus=task1.join('\r\n')
+        }
+        console.log(taskStatus)
+           sendMail(email,name,taskStatus)
+    })
+    res.send('EOD SENT')
+    })
 } 
